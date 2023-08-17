5486e16a440360659bffaf77db8aae0c607b0783
Matt Webb <matt@interconnected.org>
2023-08-17 11:28:07 +0100

    Adds tldraw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { Tldraw } from "@tldraw/tldraw";
+import "@tldraw/tldraw/tldraw.css";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6">
       <h1 className="text-6xl font-bold">Hello, Dolphin.</h1>
+      <div className="fixed inset-0">
+        <Tldraw />
+      </div>
     </main>
   );
 }
